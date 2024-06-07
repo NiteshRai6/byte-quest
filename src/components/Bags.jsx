2791625021b2a447496f992bc58e0feb60a168b3
Nitesh <ndelhi66@gmail.com>
2024-06-07 12:28:49 +0530

    API Integration Done

--- src/components/Bags.jsx
+++ src/components/Bags.jsx
@@ -1,19 +1,36 @@
-import React from 'react';
+'use client';
+
+import React, { useEffect, useState } from 'react';
 import Cards from './Cards';
+import axios from 'axios';
 
 export default function Bags() {
-    return (
-        <>
-            <div>
-                <h2>Bags . Backpacks</h2>
-                <div className='flex flex-wrap p-10 justify-center items-center'>
-                    <Cards />
-                </div>
-            </div>
-            <div className='m-20'>
-                <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos vel laudantium aut, aliquid ducimus assumenda illo culpa, voluptates dolores eum voluptate autem quos porro, corrupti quo libero incidunt optio! Beatae!</h1>
-            </div>
-        </>
+    const [cardData, setCardData] = useState();
 
+    async function getCardData() {
+        const { data: data } = await axios.get('https://fakestoreapi.com/products');
+        // console.log(data);
+        setCardData(data);
+    }
+
+    useEffect(() => {
+        getCardData();
+    }, []);
+
+    console.log(cardData);
+
+    return (
+        <div>
+            <h2 className='m-10'>Bags . Backpacks</h2>
+            <div className='flex flex-wrap justify-center items-center gap-x-10'>
+                {cardData?.map(card => (
+                    <Cards
+                        key={card?.id}
+                        card={card}
+                    />
+                ))}
+
+            </div>
+        </div>
     )
 }
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
 
-export default function Cards() {
+export default function Cards({ card }) {
     return (
-        <div className='w-[280px] h-[483px] shadow-xl rounded-xl'>
-            <img src="/card-bag.png" alt="card-bag" />
+        <div className='w-[280px] h-[483px] shadow-xl rounded-xl relative'>
+            <img
+                src="/svg/bookmark.svg"
+                alt="bookmark"
+                className='absolute top-3 right-5'
+            />
+            <img
+                src={card?.image}
+                alt="card-bag"
+                className='w-[280px] h-80'
+            />
             <div className='flex flex-col justify-center bg-[#141414] p-5 gap-2'>
-                <h2 className='text-xl text-center font-medium'>The Metro Movers Black</h2>
+                <h2 className='text-xl text-center font-medium'>
+                    {card?.title.substring(0, 20)}
+                </h2>
                 <div className='flex justify-between'>
                     <div className='flex gap-1 justify-center items-center'>
-                        <span className='text-xl'>&#8377;4899</span>
+                        <span className='text-xl'>&#8377;{card?.price}</span>
                         <span className='text-xs mt-2'>8999</span>
                         <span className='text-xs text-green-500 mt-2'>&#40;50%off&#41;</span>
                     </div>
